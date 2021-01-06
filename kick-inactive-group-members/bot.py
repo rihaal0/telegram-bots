96bf3610af2028d8b9c5adfe42ddbae90d29f752
rihaal0 <75260785+rihaal0@users.noreply.github.com>
2021-01-06 21:55:32 +0530

    Update bot.py

--- kick-inactive-group-members/bot.py
+++ kick-inactive-group-members/bot.py
@@ -4,9 +4,9 @@ from pyrogram import Client
 plugins = dict(root="plugins")
 app = Client(
      'inkick-bot',
-      bot_token = Config.BOT_TOKEN,
-      api_id = Config.APP_ID,
-      api_hash = Config.API_HASH,
+      bot_token = Config.1513564664:AAHXjEh-Em7natUk7si8-wQdodrVG5rBWP0,
+      api_id = Config.2504577,
+      api_hash = Config.fd1c39658dc2736f41b006ad55b0dcae,
       plugins = plugins
 )
 app.run()
